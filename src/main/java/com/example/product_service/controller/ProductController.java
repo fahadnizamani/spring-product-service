1bb93123c9c112df0145734a6eb928cd33258ed8
fahadnizamani <fahadnizamani1988@gmail.com>
2026-07-27 15:42:19 -0400

    Register product service with Eureka and support multiple instances

--- src/main/java/com/example/product_service/controller/ProductController.java
+++ src/main/java/com/example/product_service/controller/ProductController.java
@@ -3,6 +3,7 @@ package com.example.product_service.controller;
 import com.example.product_service.dto.ProductDTO;
 import com.example.product_service.entity.Product;
 import com.example.product_service.service.ProductService;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,13 +13,20 @@ import java.util.List;
 public class ProductController {
 
     private final ProductService service;
+    private final Environment environment;
 
-    public ProductController(ProductService service) {
+    public ProductController(ProductService service, Environment environment) {
+
         this.service = service;
+        this.environment = environment;
     }
 
     @GetMapping
     public List<ProductDTO> getAll() {
+        String port = environment.getProperty("local.server.port");
+        System.out.println(
+                ">>> GET /api/products served by PRODUCT-SERVICE on port: " + port
+        );
         return service.getAllProducts();
     }
 
